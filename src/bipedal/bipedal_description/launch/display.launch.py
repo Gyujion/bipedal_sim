eaf8870ed0f6995c38f88583d3dc241d860aacda
Gyujion <sssgggjjj4895@gmail.com>
2026-08-20 09:00:35 +0900

    feat: initial commit launch_file, urdf_file, package

--- src/bipedal/bipedal_description/launch/display.launch.py
+++ src/bipedal/bipedal_description/launch/display.launch.py
@@ -1,21 +1,21 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
     pkg_name = 'bipedal_description'
 
-    xacro_file = os.path.join(get_package_share_directory(pkg_name), 'urdf', 'bipedal_urdf.xacro')
-    doc = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': doc.toxml()}
-
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(pkg_name),'launch','rsp.launch.py'
+        )]),
+        launch_arguments={'use_sim_time':'true'}.items()
     )
 
     node_joint_state_publisher_gui = Node(
@@ -24,15 +24,28 @@ def generate_launch_description():
         name='joint_state_publisher_gui'
     )
 
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )])
+    )
     node_rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen'
     )
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'bipedal_robot'],
+        output='screen'
+    )
 
     return LaunchDescription([
-        node_robot_state_publisher,
+        rsp,
+        gazebo,
+        spawn_entity,
         node_joint_state_publisher_gui,
         node_rviz
     ])
